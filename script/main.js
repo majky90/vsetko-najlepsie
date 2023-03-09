b991e5e5de4620f142c49877d4a7e1f9f891452a
majky <varga@vargamarian.sk>
2023-03-09 07:43:40 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -27,16 +27,9 @@ const fetchData = () => {
 // Animation Timeline
 const animationTimeline = () => {
   // Spit chars that needs to be animated individually
-  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
-  const hbd = document.getElementsByClassName("wish-hbd")[0];
+  //const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+  //const hbd = document.getElementsByClassName("wish-hbd")[0];
 
-  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
-
-  hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
-    .join("</span><span>")}</span`;
 
   const ideaTextTrans = {
     opacity: 0,
